60718cf62ec143680eb9653ab240bef1815f3f9d
PierreTL <pierre.tampon@gmail.com>
2024-01-17 18:29:17 +0100

    Redesign de la navigation et des rubrique

---
+++ js/menu-burger.js
@@ -0,0 +1,26 @@
+let burgerBtn = document.querySelector('.js-burger-btn');
+let burgerMenu = document.querySelector('.js-burger-menu');
+let navigation = document.querySelector('.js-nav');
+let contenu = document.getElementById('contenu');
+let hideClassMenu = 'hide-for-small-only';
+
+console.log(burgerBtn);
+
+burgerBtn.addEventListener('click', function() {
+
+    burgerBtn.classList.toggle('v-rotation');
+    contenu.classList.toggle('hide');
+    navigation.classList.toggle('v-open-menu')
+
+    if (burgerMenu.classList.contains(hideClassMenu)) {
+        
+        burgerMenu.classList.remove(hideClassMenu);
+        burgerBtn.setAttribute('aria-expanded', true);
+
+    } else {
+        burgerMenu.classList.add(hideClassMenu);
+        burgerBtn.setAttribute('aria-expanded', false);
+
+    }
+
+});
