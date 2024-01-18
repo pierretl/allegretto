8ec47382bc29c4dd7d703dbe4fb8f565d83c362a
PierreTL <pierre.tampon@gmail.com>
2024-01-18 23:19:45 +0100

    Ajout de donnée à la liste des séjours

--- action/sejour-ajout.php
+++ action/sejour-ajout.php
@@ -129,7 +129,8 @@ if (
     $data[$lengthData]["departReel"] = securite_saisi($depart);
     $data[$lengthData]["commentaire"] = securite_saisi($commentaire);
     $data[$lengthData]["pourFamille"] = $familleUtilisateur;
-    $data[$lengthData]["parUtilisateur"] = $mailUtilisateur;
+    $data[$lengthData]["parUtilisateur"] = $utilisateur;
+    $data[$lengthData]["mailDemandeur"] = $mailUtilisateur;
     $data[$lengthData]["backgroundColor"] = $couleurEnAttenteValidation;
     $data[$lengthData]["validation"] = $validations;
 
