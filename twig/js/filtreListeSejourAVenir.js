02004690232ca1c3e3e1ad37d1f7ce5dd162d18d
PierreTL <pierre.tampon@gmail.com>
2024-01-22 13:35:09 +0100

    Refonte de l'intégration des scripts js

---
+++ twig/js/filtreListeSejourAVenir.js
@@ -0,0 +1,54 @@
+let 
+    liste = document.querySelector('.js_listeAFiltre'),
+    listeSejours = liste.querySelectorAll('li'),
+    familleUtilisateur = liste.dataset.famille;
+    btnFiltrePerso = document.querySelector('.js_filtrePerso'),
+    btnFiltreAutres = document.querySelector('.js_filtreAutres'),
+    btnFiltreTous = document.querySelector('.js_filtreTous'),
+    allBtn = [btnFiltrePerso,btnFiltreAutres,btnFiltreTous]
+;
+
+btnFiltrePerso.addEventListener('click', filtreLesSejours, false);
+btnFiltrePerso.typeFiltre = 'perso';
+
+btnFiltreAutres.addEventListener('click', filtreLesSejours, false);
+btnFiltreAutres.typeFiltre = 'autres';
+
+btnFiltreTous.addEventListener('click', filtreLesSejours, false);
+btnFiltreTous.typeFiltre = 'tous';
+
+
+function filtreLesSejours(evt) {
+    allBtn.forEach(function(boutons) {
+        boutons.classList.add('v_off');
+        boutons.setAttribute("aria-pressed", false);
+    });
+
+    switch (evt.currentTarget.typeFiltre) {
+        case 'autres':
+            listeSejours.forEach(function(sejour) {
+                sejour.classList.add('hide');
+                if (sejour.dataset.famille != familleUtilisateur) {
+                    sejour.classList.remove('hide');
+                }
+            });
+            this.classList.remove('v_off');
+            this.setAttribute("aria-pressed", true);
+            break;
+        case 'perso':
+            listeSejours.forEach(function(sejour) {
+                sejour.classList.add('hide');
+                if (sejour.dataset.famille == familleUtilisateur) {
+                    sejour.classList.remove('hide');
+                }
+            });
+            this.classList.remove('v_off');
+            this.setAttribute("aria-pressed", true);
+            break;
+        default:
+            listeSejours.forEach((sejour) => sejour.classList.remove('hide'));
+            this.classList.remove('v_off');
+            this.setAttribute("aria-pressed", true);
+            
+    }
+}
---
+++ twig/js/suppressionSejour.js
@@ -0,0 +1,6 @@
+function suppressionSejour(key) {
+    document.getElementById('suppressionSejour-'+key).showModal();
+}
+function fermeModal(key) {
+    document.getElementById('suppressionSejour-'+key).close();
+}
