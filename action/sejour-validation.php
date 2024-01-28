c9d5426b04bf6836132838e210f5c3bacf2d13ae
PierreTL <pierre.tampon@gmail.com>
2024-01-28 12:03:01 +0100

    Refonte de fonction de validation

--- action/sejour-validation.php
+++ action/sejour-validation.php
@@ -15,38 +15,57 @@ $jsonSejour = "../".getenv('DATA_SEJOUR');
 
 //recupère les valeurs nécessaire
 $pageRedirection = isset($_POST['page']) ? $_POST['page'] : 'dashboard';
-$key = $_GET['key'] - 1; // -1 car la loop de twig commence à 1
+$sejourDataAjout = urldecode($_GET['dataAjout']);
 $famille = $_SESSION['utilisateur']['famille'];
 
 //liste des séjours
 $data = getDataJson($jsonSejour);
 
-// Vérifie si la key existe pas dans les datas
-if (!isset($data[$key])) {
+//On récupère les datas du séjour a validé
+$dataDuSejour = [];
+for ($i=0; $i < count($data); $i++) {
+    if ($data[$i]['dataAjout'] == $sejourDataAjout  ) {
+        array_push($dataDuSejour, $data[$i]); 
+    }
+}
+
+// si pas de data, redirection
+if ($dataDuSejour === []) {
     header("location:../index.php?p=".$pageRedirection);
     exit;
-} 
+}
 
 // récupére la key coorespondent à la famille dans le tableau validation
-$keyFamilleInValidation = array_search($famille,array_column($data[$key]['validation'], 'id'));
+$keyFamilleInValidation = array_search($famille,array_column($dataDuSejour[0]['validation'], 'id'));
 
 // Ajoute l'heure actuel 
-$data[$key]['validation'][$keyFamilleInValidation]['accord'] = date("Y-m-d H:i:s");
+$dataDuSejour[0]['validation'][$keyFamilleInValidation]['accord'] = date("Y-m-d H:i:s");
 
 //vérifie si toute les familles on donnée leur accord
 $all_accord = true;
-for ($i=0; $i < count($data[$key]['validation']); $i++) {
-    if ( $data[$key]['validation'][$i]['accord'] == '' ) {
+for ($i=0; $i < count($dataDuSejour[0]['validation']); $i++) {
+    if ( $dataDuSejour[0]['validation'][$i]['accord'] == '' ) {
         $all_accord = false;
     }
 }
 if ($all_accord === true) {
-    $data[$key]['etat'] = getenv('CODE_SEJOUR_VALIDE');//change l'état du séjour
-    $data[$key]['backgroundColor'] = getenv('CALENDRIER_BGCOLOR-VALIDE');//passe la couleur du séjour en validé
+    $dataDuSejour[0]['etat'] = getenv('CODE_SEJOUR_VALIDE');//change l'état du séjour
+    $dataDuSejour[0]['backgroundColor'] = getenv('CALENDRIER_BGCOLOR-VALIDE');//passe la couleur du séjour en validé
 } 
 
+//créer une nouvelle liste sans l'entrée correspondant au séjour a valider
+$tousLesAutreSejour = [];
+for ($i=0; $i < count($data); $i++) {
+    if ($data[$i]['dataAjout'] != $sejourDataAjout  ) {
+        array_push($tousLesAutreSejour, $data[$i]); 
+    }
+}
+
+//fusionne les tableaux
+$dataUpdate = array_merge($tousLesAutreSejour, $dataDuSejour);
+
 //met a jour le json
-updateJason($jsonSejour, $data);
+updateJason($jsonSejour, $dataUpdate);
 
 header("location:../index.php?p=".$pageRedirection);
 
